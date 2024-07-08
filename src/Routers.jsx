0de8afd68071f6a78e90311a44fe5077ad8dbaa9
Harsh <harshsaparia007@gmail.com>
2024-07-08 22:32:11 +0530

    Added / route

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import MilkingMachines from './pages/Products/MilkingMachines';
-import Contact from './pages/Contact/Contact';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MilkingMachines from "./pages/Products/MilkingMachines";
+import Contact from "./pages/Contact/Contact";
 
 const Routers = () => {
     return (
         <Routes>
-            <Route path='/farmosys' element={<Home />}></Route>
-            <Route path='/farmosys/milking-machines' element={<MilkingMachines />} />
-            <Route path='/farmosys/contact-us' element={<Contact />} />
-            <Route path='*' element={<p>No page found</p>} />
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/farmosys" element={<Home />}></Route>
+            <Route
+                path="/farmosys/milking-machines"
+                element={<MilkingMachines />}
+            />
+            <Route path="/farmosys/contact-us" element={<Contact />} />
+            <Route path="*" element={<p>No page found</p>} />
         </Routes>
     );
 };
